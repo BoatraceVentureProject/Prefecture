3d717af1a571bf862bea2a3d2ae8c2ff475603ea
shimomo <developer@shimomo.net>
2025-01-20 04:34:50 +0900

    Add BaseTestCase.php to tests for prefecture tests

---
+++ tests/BaseTestCase.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Boatrace\Venture\Project\Tests;
+
+use Boatrace\Venture\Project\Tests\DataTransferObjects\PrefectureDataTransferObject as PrefectureDTO;
+use Illuminate\Support\Collection;
+use PHPUnit\Framework\TestCase as PHPUnitTestCase;
+
+/**
+ * @author shimomo
+ */
+class BaseTestCase extends PHPUnitTestCase
+{
+    /**
+     * @var \Illuminate\Support\Collection
+     */
+    protected Collection $prefecturesDTO;
+
+    /**
+     * @return void
+     */
+    public function setUp(): void
+    {
+        $this->initializePrefecture();
+    }
+
+    /**
+     * @return void
+     */
+    protected function initializePrefecture(): void
+    {
+        if (! isset($this->prefecturesDTO)) {
+            $this->prefecturesDTO = collect();
+            foreach (require __DIR__ . '/../config/prefectures.php' as $prefecture) {
+                $this->prefecturesDTO->put($prefecture['id'], new PrefectureDTO(
+                    $prefecture['id'],
+                    $prefecture['name'],
+                    $prefecture['short_name'],
+                    $prefecture['hiragana_name'],
+                    $prefecture['katakana_name'],
+                    $prefecture['english_name']
+                ));
+            }
+        }
+    }
+
+    /**
+     * @param  \Boatrace\Venture\Project\Tests\DataTransferObjects\PrefectureDataTransferObject  $prefecture
+     * @param  \Illuminate\Support\Collection                                                    $collection
+     * @param  string|int                                                                        $keyName
+     * @return void
+     */
+    protected function assertPrefecture(PrefectureDTO $prefecture, Collection $collection, string|int $keyName): void
+    {
+        $this->assertSame($prefecture->id, $collection->get($keyName)->get('id'));
+        $this->assertSame($prefecture->name, $collection->get($keyName)->get('name'));
+        $this->assertSame($prefecture->shortName, $collection->get($keyName)->get('short_name'));
+        $this->assertSame($prefecture->hiraganaName, $collection->get($keyName)->get('hiragana_name'));
+        $this->assertSame($prefecture->katakanaName, $collection->get($keyName)->get('katakana_name'));
+        $this->assertSame($prefecture->englishName, $collection->get($keyName)->get('english_name'));
+    }
+}
